4427eab70bc4a93603e58dcc5c5ebe898529a0a3
krilen <krister@lenberg.se>
2025-01-09 15:06:45 +0100

    Fix detail about train

--- e4_advanced.py
+++ e4_advanced.py
@@ -11,15 +11,16 @@ distance = 470
 try: 
     speed = int(input("What speed, in km/h, do you want to travel between Goteborg and Stockholm? >> "))
     
-    print(f"Resan kommer att ta {(distance / speed):.1f} timmar.")
-    print(f"Resan kommer att ta {((distance / speed)*60*60):.0f} sekunder.")
-    print(f"Resan kommer att ta {((distance / speed)*60):.0f} minuter.")
+    print(f"The journey will take {(distance / speed):.1f} hour{ 's' if (distance / speed) > 1 else ''}.")
+    print(f"The journey will take {((distance / speed)*60*60):.0f} seconds.")
+    print(f"The journey will take {((distance / speed)*60):.0f} minutes.")
     
-    train_seconds = (distance / speed) *60 *60
+    train_seconds = int((distance / speed) *60 *60)
     train_hours = train_seconds // (60*60)
-    train_min = (train_seconds % (60*60)) / 60
+    train_min = math.ceil((train_seconds % (60*60)) / 60)
+        
+    print(f"The journey will take {train_hours} hour{ '' if train_hours == 1 else 's'} and {train_min} minute{ 's' if train_min != 1 else ''}.")
     
-    print(f"Resan kommer att ta {train_hours:.0f} timmar och {train_min:.0f} minuter.")
 except:
     print("Sorry the train got stuck")
     
@@ -43,20 +44,27 @@ try:
     
     side_x = math.sqrt(side_a * side_a + side_b * side_b)
     
-    print(f"Side X is of lenght: {side_x:.1f}.")
-    
 except:
     print("Sorry that is an square")
     
+else:
+    print(f"Side X is of lenght: {side_x:.1f}.")
+    
+finally:
+    pass
     
 """
 3A
 Skriv ett program som talar om dagens datum.
 """
 
-date_today = datetime.datetime.today().strftime('%Y%b%d')
-date_next_week = (datetime.datetime.strptime(date_today , "%Y%b%d") + datetime.timedelta(days=7)).strftime('%Y%b%d')
+date_format = "%Y%b%d"
 
-print(f"\nTodays is: {date_today}")
-print(f"In 7 days it will be: {date_next_week}.")
+days_ahead = 10
+
+date_today = datetime.datetime.today()
+date_next_week = (datetime.datetime.strptime(date_today.strftime(date_format), date_format) + datetime.timedelta(days=days_ahead))
+
+print(f"\nTodays date: {date_today.strftime(date_format)}")
+print(f"In {days_ahead} days the date will be: {date_next_week.strftime(date_format)}.")
 
